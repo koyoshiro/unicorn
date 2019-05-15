2c18f4aa966e63ed2ec62a77c7fe0c6403a40ba9
jl.gu <jl.gu@dst67680.local>
2019-05-15 22:37:04 +0800

    终于有眉目了

--- dest/View/index.js
+++ dest/View/index.js
@@ -1,25 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = require("react");
-const inject = (viewModel) => {
-    return function withStore(wrappedComponent) {
-        class StoreWrapper extends react_1.Component {
-            constructor(props) {
-                super(props);
-            }
-            render() {
-                return react_1.default.createElement("wrappedComponent", { viewModel: viewModel });
-            }
-            componentDidMount() {
-                viewModel.registerView(this);
-            }
-            shouldComponentUpdate() {
-                //return false; // 模块只能被初始化一次，不允许更新
-            }
+const connect = (viewModel, renderComponent) => {
+    class StoreWrapper extends react_1.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+                vm: viewModel
+            };
         }
-        return StoreWrapper;
-    };
+        render() {
+            return react_1.createElement("renderComponent", { viewModel: this.state.vm });
+        }
+        componentDidMount() {
+            viewModel.registerView(this);
+        }
+        doSomething(viewModel) {
+            this.setState({ vm: viewModel });
+        }
+        shouldComponentUpdate() {
+            //return false; // 模块只能被初始化一次，不允许更新
+        }
+    }
+    return StoreWrapper;
 };
-exports.inject = inject;
-const connect = (viewModel, component) => {
-};
+exports.connect = connect;
