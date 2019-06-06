67ff6162de4de16eab300a1e18603cd67d56c859
jl.gu <jl.gu@dst67680.local>
2019-06-06 12:01:09 +0800

    广播功能补全

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import { I_UC_Builder } from './Interface/I_UC_Builder';
+import { I_Broadcast_Subject } from './Interface/I_Broadcast_Subject';
 import Builder from './Builder';
-// import Broadcast from './Broadcast';
+import Broadcast from './Broadcast';
 export default class Unicorn {
     protected __BUILD_RECORD__: string[] = []; //Set<string>[] = new Set;
     protected __BUILD_STACK__: any[] = [];
-    // private Broadcast : any;
+    private Broadcast: any;
 
     constructor() {
         // todo init Log
         // this.__BUILD_RECORD__ = [];
         // this.__BUILD_STACK__ = [];
-        // this.Broadcast = new Broadcast();
+        this.Broadcast = new Broadcast();
     }
 
     public builder(builderParams: I_UC_Builder) {
         if (builderParams && builderParams.namespace && this.__BUILD_RECORD__.indexOf(builderParams.namespace) === -1) {
             this.__BUILD_RECORD__.push(builderParams.namespace);
-            const builderInstance = new Builder(builderParams, this.tunnel);
+            const builderInstance = new Builder(builderParams, this.tunnel, this.signal);
             this.__BUILD_STACK__.push({
                 key: builderParams.namespace,
                 instance: builderInstance
@@ -33,7 +34,20 @@ export default class Unicorn {
         builderChannel.UCViewModel.dispatch(actionName, payload);
     }
 
-    // private _broadcast(){
-    //     const broadcastObj = new Broadcast();
-    // }
+    private signal(): any {
+        const doSubscribe = (behaviorName: string): void => {
+            const broadcastSubscribe: I_Broadcast_Subject | undefined = this.Broadcast.getSubscribe(behaviorName);
+            if (broadcastSubscribe) {
+                const builderChannel = this.__BUILD_STACK__.find(
+                    (builder: any) => builder.key === broadcastSubscribe.builderName
+                ).instance;
+                builderChannel.UCViewModel.dispatch(broadcastSubscribe.actionName, broadcastSubscribe.payload);
+            }
+        };
+        return {
+            subscribe: this.Broadcast.subscribe,
+            unSubscribe: this.Broadcast.unSubscribe,
+            doSubscribe: { doSubscribe }
+        };
+    }
 }
